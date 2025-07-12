d7538a1bd0a89861fbaf3f9717d5164ed19a35fd
thechhatrapal <chhatrapalgtanwar@gmail.com>
2025-07-12 20:23:19 +0530

    added new project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+const port = 4000;
+
+app.get('/',  (req, res) => {
+    res.json({msg: 'hello'})
+})
+
+app.listen(port, () => console.log(`server is running on port 4000`))
