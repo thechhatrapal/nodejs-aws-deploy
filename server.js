30dcdcdef576e76bf3c447ac3c3afcdfda70b79d
thechhatrapal <chhatrapalgtanwar@gmail.com>
2025-07-14 00:02:46 +0530

    added server file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json())
 const port = 4000;
 
 app.get('/',  (req, res) => {
-    res.json({msg: 'hello'})
+    res.json({msg: 'hello, I am changed something'})
 })
 
 app.listen(port, () => console.log(`server is running on port 4000`))
